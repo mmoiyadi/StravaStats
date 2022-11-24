2199c2cba71995e9fb091c3bf5cf529520ca1009
Mohammed Moiyadi <mmoiyadi@wideorbit.com>
2022-11-24 20:02:40 +0530

    Initial Commit

---
+++ StravaStats/Options/StravaConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace StravaStats.Options
+{
+    public class StravaConfig
+    {
+        public const string StravaConfigConst = "StravaConfig";
+        public long ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string RefreshToken { get; set; }
+        public long AthleteId { get; set; }
+    }
+}
